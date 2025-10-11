450bc1ac40e0ba09fdb939f2100d12a5928ff4a7
Nicholas <nicholwaithe11@gmail.com>
2025-10-10 20:36:52 -0400

    Add files via upload
    Signed-off-by: Nicholas  <nicholwaithe11@gmail.com>

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BUtext'
+copyright = '2025, Gregory Hallenbeck, Nicholas Waithe, Matthew Reardon, William Harris'
+author = 'Gregory Hallenbeck, Nicholas Waithe, Matthew Reardon, William Harris'
+release = '10/10/2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
